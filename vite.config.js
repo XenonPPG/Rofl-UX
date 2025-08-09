bf74fe0dcdae95c13e7e02e44ebae098de32c946
ART <popugajarcik@gmail.com>
2025-08-09 22:07:28 +0300

    path fix 2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Super-UX/',
+  base: '/Rofl-UX/',
   plugins: [
     vue(),
     vueDevTools(),
